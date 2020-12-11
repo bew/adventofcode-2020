312100b84eef5fd2d824296ec1a79de637ae5ca9
Benoit de Chezelles <benoit.dechezelles@gmail.com>
2020-12-11 19:16:30 +0100

    main: Use static array; Extract usage; Make is more readable

--- src/main.rs
+++ src/main.rs
@@ -9,41 +9,47 @@ mod day02;
 
 type DayFn = fn() -> ();
 
-fn main() {
-    let day_funcs: Vec<(&str, DayFn)> = vec![ // using a vec to keep correct order
-        ("day01", day01::solve),
-        ("day02", day02::solve),
-    ];
+// We use a slice of the array to not have to specify its size in the type.
+// See: https://stackoverflow.com/questions/23810032/how-to-specify-const-array-in-global-scope-in-rust
+static DAYS: &[(&str, DayFn)] = &[
+    // using a vec to keep correct order
+    ("day01", day01::solve),
+    ("day02", day02::solve),
+];
 
-    let mut prog_args = env::args();
-    let prog_name = prog_args.next().unwrap_or("prog".to_string());
-    match prog_args.next() {
-        Some(is_all) if is_all == "all" => {
-            for day_entry in day_funcs {
-                println!("--- {}", day_entry.0);
-                day_entry.1();
+fn print_usage() {
+    let prog_name = env::args().next().unwrap_or("prog".to_string());
+    let day_names: Vec<_> = DAYS.iter().map(|e| e.0).collect();
+    println!("Usage: {} <day>", prog_name);
+    println!(
+        "Where <day> is either 'all' or one of: {}",
+        day_names.join(", ")
+    );
+    exit(1);
+}
+
+fn main() {
+    let prog_args: Vec<String> = env::args().collect();
+    let first_arg = prog_args.get(1);
+    // Converts Option<String> to Option<&str> so I can match on Some("all")
+    // (necessary because matching on Some("all".to_string()) does not work).
+    match first_arg.and_then(|s| Some(s.as_str())) {
+        Some("all") => {
+            for day in DAYS {
+                println!("--- {}", day.0);
+                day.1();
             }
-        },
+        }
         Some(wanted_day) => {
-            let matching_day = day_funcs.iter()
-                .find(|day_entry| day_entry.0 == wanted_day);
+            let matching_day = DAYS.iter().find(|day| day.0 == wanted_day);
             match matching_day {
-                Some(day_entry) => day_entry.1(),
+                Some(day) => day.1(),
                 None => {
                     println!("Unknown day '{}'", wanted_day);
                     exit(1);
                 }
             };
-        },
-        None => {
-            println!("Usage: {} <day>", prog_name);
-            println!(
-                "Where <day> is either 'all' or one of: {}",
-                day_funcs.iter()
-                    .map(|e| e.0).collect::<Vec<&str>>()
-                    .join(", ")
-            );
-            exit(1);
         }
+        None => print_usage(),
     };
 }
